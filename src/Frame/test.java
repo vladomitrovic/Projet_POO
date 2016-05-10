ee2bf92877685caaec5cf6544afbdea73fbf8533
Vlado <Vlado@Vlado>
2016-05-10 14:47:03 +0200

    fff

--- src/Frame/test.java
+++ src/Frame/test.java
@@ -2,19 +2,19 @@ package Frame;
 
 public class test {
 //afdgagfgt
-	private int salut ;
+	private String salut ;
+	
+	public test(String salut){
+		this.salut = salut ;
+	}
 	
 	public int bonjour(){
 		System.out.println("salut");
 		return 4;
 		
-	}	
-		
-		
-		public int test(){
-			return 0;
-			
-		}
-		
+	}
 	
-}
+	
+	
+	}
+	
