02d6d5f602638f2b4cff9d80f9bc125391048d1b
Vlado <Vlado@Vlado>
2016-05-11 17:06:28 +0200

    launcher

--- src/Frame/Launcher.java
+++ src/Frame/Launcher.java
@@ -1,22 +1,23 @@
 package Frame;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class Launcher extends JFrame{
 
 	private JButton contact=new JButton("Contacts");
 	private JButton galerie=new JButton("Galerie");
 	private JButton inconnu=new JButton("inconnu");
+	private JPanel panel=new JPanel();
+	
+	private ImageIcon wallPaper=new ImageIcon("C:/Users/Vlado/Desktop/wallpaper.JPEG");
 	
 	
 	
 	public Launcher(){
 
 		//Affichage de la frame
+		
 		setPreferredSize(new Dimension(480, 800));
 		setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -24,16 +25,30 @@ public class Launcher extends JFrame{
 		setLocationRelativeTo(null);
 		
 		
-		//Ajout des bouttons
-		add(contact, BorderLayout.NORTH);
-		add(galerie, BorderLayout.CENTER);
-		add(inconnu, BorderLayout.SOUTH);
+		
+		contact.setAlignmentX(Component.CENTER_ALIGNMENT);
+		galerie.setAlignmentX(Component.CENTER_ALIGNMENT);
+		inconnu.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
 		
-		contact.setPreferredSize(new Dimension(480, 266));
-		galerie.setPreferredSize(new Dimension(480, 267));
-		inconnu.setPreferredSize(new Dimension(480, 266));
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(contact);
+		panel.add(galerie);
+		panel.add(inconnu);
+		add(panel);
 		
+		
+		
+		
+//		//Ajout des bouttons
+//		add(contact, BorderLayout.NORTH);
+//		add(galerie, BorderLayout.CENTER);
+//		add(inconnu, BorderLayout.SOUTH);
+//		
+//		
+//		contact.setPreferredSize(new Dimension(100, 50));
+//		galerie.setPreferredSize(new Dimension(100, 50));
+//		inconnu.setPreferredSize(new Dimension(100, 50));		
 		
 		
 	}
