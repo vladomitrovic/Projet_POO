376e3dad4280180693aa8db3e22a15fc5f93db35
Vlado <Vlado@Vlado>
2016-05-11 16:21:57 +0200

    Début contact

--- src/Contact/Contact.java
+++ src/Contact/Contact.java
@@ -2,8 +2,12 @@ package Contact;
 
 public class Contact {
 
+	private String nom, prenom, tel;
 	
-	public Contact(){
+	public Contact(String nom, String prenom, String tel){
+		this.nom=nom;
+		this.prenom=prenom;
+		this.tel=tel;
 		
 	}
 }
