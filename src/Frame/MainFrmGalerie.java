b55ecea03b681e366a69fad69c0a49871d879816
Aleksandar <Aleksandar@10.91.129.61>
2016-05-16 17:17:47 +0200

    Sranje

--- src/Frame/MainFrmGalerie.java
+++ src/Frame/MainFrmGalerie.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -29,26 +30,17 @@ public class MainFrmGalerie extends BasiqueFrame {
 
 	private JPanel containerPhotos = new JPanel();
 	private JPanel upPanel = new JPanel();
-
 	private Galerie galerie = new Galerie();
-
-	private ImageIcon photo1 = new Photo(
-			new ImageIcon("Pictures/animal1.jpeg").getImage().getScaledInstance(123, 130, Image.SCALE_DEFAULT));
-
 	private JButton[] buttons = new JButton[50];
 
-	private ImageIcon photo2 = new Photo(
-			new ImageIcon("Pictures/paysage1.jpeg").getImage().getScaledInstance(123, 130, Image.SCALE_DEFAULT));
-	private ImageIcon photo3 = new Photo(
-			new ImageIcon("Pictures/animal3.jpeg").getImage().getScaledInstance(123, 130, Image.SCALE_DEFAULT));
-	private ImageIcon photo4 = new Photo(
-			new ImageIcon("Pictures/paysage3.jpeg").getImage().getScaledInstance(123, 130, Image.SCALE_DEFAULT));
-	private ImageIcon photo5 = new Photo(
-			new ImageIcon("Pictures/animal2.jpg").getImage().getScaledInstance(123, 130, Image.SCALE_DEFAULT));
-	private ImageIcon photo6 = new Photo(
-			new ImageIcon("Pictures/paysage2.jpeg").getImage().getScaledInstance(123, 130, Image.SCALE_DEFAULT));
-	private ImageIcon photo7 = new Photo(
-			new ImageIcon("Pictures/ville1.jpeg").getImage().getScaledInstance(123, 130, Image.SCALE_DEFAULT));
+	
+	private Photo photo1 = new Photo("Pictures/animal1.jpeg");
+	private Photo photo2 = new Photo("Pictures/paysage1.jpeg");
+	private Photo photo3 = new Photo("Pictures/paysage3.jpeg");
+	private Photo photo4 = new Photo("Pictures/paysage3.jpeg");
+	private Photo photo5 = new Photo("Pictures/animal2.jpg");
+	private Photo photo6 = new Photo("Pictures/paysage2.jpeg");
+	private Photo photo7 = new Photo("Pictures/ville1.jpeg");
 
 	private BasicArrowButton backButton = new BasicArrowButton(BasicArrowButton.WEST);
 	private JButton addButton = new JButton("+");
@@ -59,11 +51,17 @@ public class MainFrmGalerie extends BasiqueFrame {
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
 		// set containerPhotos
-		FlowLayout flowLayout = new FlowLayout() ;
+		FlowLayout flowLayout = new FlowLayout();
 		flowLayout.setHgap(0);
 		flowLayout.setVgap(0);
 		containerPhotos.setLayout(flowLayout);
-		
+
+		// modify size of buttons
+		for (int i = 0; i < buttons.length; i++) {
+			if (buttons[i] != null)
+				buttons[i].setPreferredSize(new Dimension(130, 100));
+		}
+
 		// add images to gallery and to buttons
 		galerie.addPhoto((Photo) photo1, buttons);
 		galerie.addPhoto((Photo) photo2, buttons);
@@ -72,6 +70,7 @@ public class MainFrmGalerie extends BasiqueFrame {
 		galerie.addPhoto((Photo) photo5, buttons);
 		galerie.addPhoto((Photo) photo6, buttons);
 		galerie.addPhoto((Photo) photo7, buttons);
+		
 
 		// add listener to Jbuttons
 		for (int i = 0; i < buttons.length; i++) {
@@ -116,18 +115,26 @@ public class MainFrmGalerie extends BasiqueFrame {
 
 	class PhotoFrame extends BasiqueFrame {
 
-		private JLabel label = new JLabel();
+		private JLabel label;
 
-		public PhotoFrame(Object source) {
+		public PhotoFrame(Photo photo) {
+			// photo = new Photo(photo.getPath(),new Image())
+			System.out.println(photo.getPath());
+			label = new JLabel();
+			label.setIcon(photo);
+			add(label);
 		}
-
 	}
 
 	class Photo_Click implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent arg0) {
+		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			JButton button = (JButton) e.getSource();
+			setVisible(false);
+			PhotoFrame photoFrame = new PhotoFrame((Photo) button.getIcon());
+			photoFrame.setVisible(true);
 
 		}
 
