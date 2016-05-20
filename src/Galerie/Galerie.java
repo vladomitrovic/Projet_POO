6aa0716635ef878e1b7f2809f2a6da00461df4a6
Aleksandar <Aleksandar@AleksandarLazic>
2016-05-20 15:38:02 +0200

    Ok quand j'ajoute une photo depuis le bouton + :))

--- src/Galerie/Galerie.java
+++ src/Galerie/Galerie.java
@@ -1,29 +1,35 @@
 package Galerie;
 
+import java.awt.Dimension;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
+import Frame.MainPanelGalerie;
+
 public class Galerie {
-	
-	private Photo[] photos = new Photo[50] ;
-	private int cpt ;
-	
-	public void addPhoto(Photo photo, JButton[] buttons){
-		photos[cpt] = photo ;
-		buttons[cpt] = new JButton(photo) ;
-        buttons[cpt].setBorderPainted(false); 
-        buttons[cpt].setContentAreaFilled(false); 
-        buttons[cpt].setFocusPainted(false); 
-        buttons[cpt].setOpaque(false);
-		cpt ++ ;		
+
+	private Photo[] photos = new Photo[50];
+	private int cpt;
+
+	public void addPhoto(MainPanelGalerie panelGalerie, Photo photo, JButton[] buttons) {
+		photos[cpt] = photo;
+		buttons[cpt] = new JButton(photo);
+		buttons[cpt].setBorderPainted(false);
+		buttons[cpt].setContentAreaFilled(false);
+		buttons[cpt].setFocusPainted(false);
+		buttons[cpt].setOpaque(false);
+		buttons[cpt].setPreferredSize(new Dimension(130, 100));
+		panelGalerie.addActionListenerAndToPanel(buttons, cpt);
+		cpt++;
 	}
-	
-	public int getCpt(){
-		return cpt ;
+
+	public int getCpt() {
+		return cpt;
 	}
-	
-	public Photo[] getPhotos(){
-		return photos ;
+
+	public Photo[] getPhotos() {
+		return photos;
 	}
 
 }
