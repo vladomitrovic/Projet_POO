f5dacfd9b333b12cfe251b4152f3d021a7e998e5
Aleksandar <Aleksandar@10.91.131.159>
2016-06-14 13:50:04 +0200

    dieu sait

--- src/Elements/ContactButton.java
+++ src/Elements/ContactButton.java
@@ -3,6 +3,7 @@ package Elements;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -10,16 +11,14 @@ import java.awt.event.MouseListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import Frame.Contact.Contact_Carnet;;
 
+public class ContactButton extends JButton {
 
-public class ContactButton extends JButton{
-	
-	
-	
-	public ContactButton(String text){
-		
+	public ContactButton(String text) {
 
 		setText(text);
 		setContentAreaFilled(false);
@@ -27,26 +26,45 @@ public class ContactButton extends JButton{
 		setHorizontalAlignment(SwingConstants.LEFT);
 		setFont(new Font("Arial", Font.PLAIN, 20));
 		setPreferredSize(new Dimension(400, 40));
-		addMouseListener(new ColorButtton());
-		
-	}
-	
-	
-	class ColorButtton extends MouseAdapter{
 
-	
-
-		@Override
-		public void mouseEntered(MouseEvent e) {
-			setBackground(Color.cyan);
-		}
-
-		@Override
-		public void mouseExited(MouseEvent e) {
-			setBackground(Color.LIGHT_GRAY);
-
-		}
+		addChangeListener(new ColorChange());
 
 	}
+
+	class ColorChange implements ChangeListener {
+
+		@Override
+		public void stateChanged(ChangeEvent arg0) {
+			// TODO Auto-generated method stub
+			if (getModel().isPressed()) {
+				setBackground(Color.BLACK);
+				revalidate();
+				repaint();
+				System.out.println("black");
+			} else if (getModel().isRollover()) {
+				setBackground(Color.WHITE);
+				System.out.println("white");
+			} else {
+				setBackground(Color.BLUE);
+				System.out.println("blue");
+			}
+		}
+
+	}
+
+	// class ColorButtton extends MouseAdapter {
+	//
+	// @Override
+	// public void mouseEntered(MouseEvent e) {
+	//
+	// }
+	//
+	// @Override
+	// public void mouseExited(MouseEvent e) {
+	// setBackground(Color.LIGHT_GRAY);
+	//
+	// }
+	//
+	// }
 
 }
